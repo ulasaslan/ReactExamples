99f54802abf99be636a1f3b36d0ae061d3112f95
Ulas Aslan <aslanulas@gmail.com>
2021-05-14 15:48:22 +0300

    yeni örnek eklendi

---
+++ 11-navbar/src/Navbar.js
@@ -0,0 +1,63 @@
+import React, {useState, useRef, useEffect} from 'react'
+import {FaBars, FaTwitter} from 'react-icons/fa'
+import {links, social} from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+
+    const [showMenu, setShowMenu] = useState(false)
+    const linksContainerRef = useRef(null)
+    const linksRef = useRef(null)
+
+
+
+    const handleShowMenu = (event) => {
+        setShowMenu(!showMenu)
+    }
+
+    useEffect(()=>{
+        if(showMenu){
+            linksContainerRef.current.style.height = linksRef.current.getBoundingClientRect().height + 'px';
+        }else{
+            linksContainerRef.current.style.height = '0px';
+
+        }
+    },[showMenu])
+
+    return (
+        <nav>
+            <div className='nav-center'>
+                <div className='nav-header'>
+                    <img src={logo} className='logo' alt='logo'/>
+                    <button className='nav-toggle' onClick={handleShowMenu}>
+                        <FaBars/>
+                    </button>
+                </div>
+                <div className='links-container' ref={linksContainerRef} >
+                    <ul className='links' ref={linksRef}>
+                        {links.map((link) => {
+                            const {id, url, text} = link;
+                            return (
+                                <li key={id}>
+                                    <a href={url}>{text}</a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <ul className='social-icons'>
+                    {social.map((socialIcon) => {
+                        const {id, url, icon} = socialIcon;
+                        return (
+                            <li key={id}>
+                                <a href={url}>{icon}</a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
